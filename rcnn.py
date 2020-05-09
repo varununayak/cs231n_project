eb749ac96b160f338783c80283f1cdb16be44ce8
varununayak <varunnayak3101@gmail.com>
2020-05-08 21:17:05 -0700

    differential pose prediction

--- rcnn.py
+++ rcnn.py
@@ -12,11 +12,7 @@ WINDOW_SIZE = 5 #10
 WINDOW_IMG_SHAPE = (WINDOW_SIZE, IMG_SIZE, IMG_SIZE, 3)
 DIM_PREDICTIONS = 3 # 3 for translation, 3 more probably (euler angles) for translation + rotation
 
-def main():
-    print("----------------------- Using TensorFlow version:", tf.__version__,"---------------------------")
-    # Load ground truth
-    poses = load_poses('ground_truth_odometry/data_odometry_poses/dataset/poses/01.txt', get_only_translation=True)
-    # Load images
+def load_images():
     filelist = glob.glob('small_dataset/odometry/01/image_2/*.png')
     images = [cv.imread(fname) for fname in filelist]
     for i, image in enumerate(images):
@@ -25,11 +21,30 @@ def main():
         images[i] = image
     images = np.asarray(images)
     print("Images shape: ", images.shape)
+    return images
+
+def cumulate_poses(poses_predicted, init_pose):
+    poses_predicted_cum = [init_pose]
+    for pose_diff in poses_predicted:
+        poses_predicted_cum.append(poses_predicted_cum[-1] + pose_diff)
+    return np.asarray(poses_predicted_cum)
+
+def main():
+    print("----------------------- Using TensorFlow version:", tf.__version__,"---------------------------")
+    # Load ground truth
+    poses = load_poses('ground_truth_odometry/data_odometry_poses/dataset/poses/01.txt', get_only_translation=True)
+    # Load and preprocess images
+    images = load_images()
+    # Check some stuff for consistency
     num_train, dim_poses = poses.shape
     assert num_train == images.shape[0], "Number of images and number of poses don't match!"
     assert dim_poses == DIM_PREDICTIONS, "Dimension mismatch between pose data and network output, check loaded data!"
+    # num train after windowing
     num_train = num_train - WINDOW_SIZE + 1
-    poses = poses[-num_train:]
+    # Process poses current - previous
+    poses_original = np.copy(poses[-num_train:]) # store for later
+    init_pose = poses[-num_train-1]
+    poses = poses[-num_train:] - poses[-num_train-1:-1]
     # Create windowed dataset of images
     images_windowed = np.zeros((num_train, WINDOW_SIZE, IMG_SIZE, IMG_SIZE, 3))
     for i in range(num_train):
@@ -50,12 +65,13 @@ def main():
     model = tf.keras.models.Sequential([cnn_model_time_dist, rnn_model])
     model.summary()
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-    model.fit(images_windowed, poses, batch_size = 16, epochs = 10)
-    poses_predicted = model.predict(images_windowed)
-    print("Poses shape:", poses.shape)
+    model.fit(images_windowed, poses, batch_size = 16, epochs = 1)
+    poses_predicted = model.predict(images_windowed[0:500])
+    print("Poses shape:", poses_original.shape)
     print("Poses Pred shape:", poses_predicted.shape)
-    plt.plot(poses[:,0], poses[:,2])
-    plt.plot(poses_predicted[:,0], poses_predicted[:,2])
+    poses_predicted_cum = cumulate_poses(poses_predicted, init_pose)
+    plt.plot(poses_original[:,0], poses_original[:,2])
+    plt.plot(poses_predicted_cum[:,0], poses_predicted_cum[:,2])
     plt.show()
 
 
